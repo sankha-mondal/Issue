1595db312b1ec17528bd8cfb9bb9e6abe81a8670
Sankha Subhra Mondal <sankha123@gmail.com>
2022-06-23 12:17:17 +0530

    done

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ class Demo{
     public static void main(String args[]){  
      System.out.println("Hello Java with 1.8 version");
      System.out.println("Hello Java with 1.8 version");   
-     System.out.println("Hello Java with 1.8 version");
+
+     System.out.println("Automatically pull from Git");
+     System.out.println("Automatically pull from Git");
     }  
 }  
